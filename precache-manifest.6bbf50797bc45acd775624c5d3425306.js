14d87840578f605af82396dc977a30a3b0c6e55e
TA <pepper.ppeatns@gmail.com>
2019-05-23 14:19:55 +0900

    Updates

---
+++ precache-manifest.6bbf50797bc45acd775624c5d3425306.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794d1f4491ead3a774b658ec1af64a05",
+    "url": "/Task-Tier1/index.html"
+  },
+  {
+    "revision": "700907f86de553e35146",
+    "url": "/Task-Tier1/static/css/main.e43bb4b0.chunk.css"
+  },
+  {
+    "revision": "adf37fbd67ea0c26131b",
+    "url": "/Task-Tier1/static/js/2.7aa52c23.chunk.js"
+  },
+  {
+    "revision": "700907f86de553e35146",
+    "url": "/Task-Tier1/static/js/main.5610a7d9.chunk.js"
+  },
+  {
+    "revision": "7737102153d71c3d88ec",
+    "url": "/Task-Tier1/static/js/runtime~main.0acf60bd.js"
+  }
+]);
